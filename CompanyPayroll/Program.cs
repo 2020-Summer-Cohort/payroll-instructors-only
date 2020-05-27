1bc70129168cbd6441c4f2677ad773f9a1910a58
Jen E <jenevans3ten@gmail.com>
2020-05-27 13:40:58 -0400

    test modification

--- CompanyPayroll/Program.cs
+++ CompanyPayroll/Program.cs
@@ -6,7 +6,7 @@ namespace CompanyPayroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  It's going to be a good day.");
         }
     }
 }
